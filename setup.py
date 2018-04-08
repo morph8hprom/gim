04e111361a782e510d58453b70306eb4d20dfbdb
Noved <morph8hprom@gmail.com>
2018-04-08 15:14:44 -0500

    Fixed setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name = "GIM",
+version = '0.1.0',
+python_requires = '>=3',
+install_requires = ['cmd2>=0.8.2'],
+description = 'Item management utility for roleplaying/adventure game',
+url = 'https://www.github.com/morph8hprom/gim',
+author = 'Devon Blackburn',
+author_email = 'morph8hprom@gmail.com',
+license = 'GPL 3.0',
+packages = find_packages('src'), package_dir = {'' : 'src'},
+include_package_data = True,
+package_data = {
+'' : ['*.txt'],
+'' : ['*.json'],
+}
