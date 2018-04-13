c7897b080a8710ce6308a9bcfc5851f7f20a16ce
Noved <morph8hprom@gmail.com>
2018-04-13 13:03:24 -0500

    Added setUpClass

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,3 +3,13 @@
 import unittest
 import json
 from src.item import *
+
+class ItemAttrbituesTestCase(unittest.TestCase):
+
+    """
+    Contains tests for Item instance attributes
+    """
+    @classmethod
+    def setUpClass(cls):
+
+        pass
