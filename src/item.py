14b1d0ba612a7ef230aecc1b7c3400da5ba01c0c
Noved <morph8hprom@gmail.com>
2018-04-13 12:34:33 -0500

    Added test data and updated item.py

---
+++ src/item.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from pkg_resources import resource_string
+import json
+"""
+File used to define item class, weapon class, armor class, and consumable class
+"""
+
+class Item:
+    """
+    Parameters and methods for the base Item class
+    """
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+
+
+class Weapon(Item):
+    """
+    Parameters and methods for the Weapon subclass
+    """
+    def __init__(self, name, desc, damage, weapon_type):
+        super().__init__(name, desc)
+        self.damage = damage
+        self.weapon_type = weapon_type
+
+
+class Armor(Item):
+    """
+    Parameters and methods for the Armor subclass
+    """
+    def __init__(self, name, desc, defense, armor_slot):
+        super().__init__(name, desc)
+        self.defense = defense
+        self.armor_slot = armor_slot
+
+class Consumable(Item):
+    """
+    Parameters and methods for the Consumable subclass
+    """
+    def __init__(self, name, desc, effect):
+        super().__init__(name, desc)
+        self.effect = effect
+
+
+
+def build_item(id):
+    """
+    Gathers item data from json file and returns instance object based on
+    item type.
+    """
+    jsontext = resource_string(__name__, 'data/item{}.json'.format(id))
+    d = json.loads(jsontext.decode('utf-8'))
+    d['id'] = id
+    if d['item_type'] == 'weapon':
+        item = Weapon(**d)
