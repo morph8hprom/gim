b2101a13876a5931265e73300cc1312e3d1cea83
Noved <morph8hprom@gmail.com>
2018-04-15 12:04:12 -0500

    Added attribute tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -11,13 +11,52 @@ class ItemAttrbituesTestCase(unittest.TestCase):
     """
     @classmethod
     def setUpClass(cls):
+        """
+        Builds a dictionary of all items, a dictionary of only weapons,
+        a dictionary of only armor, and a dictionary of only consumables
+        """
         # Create variables for starting id and number of items
         start_id = 1
         num_items = 5
         # Create a variable to contain items of all type and
         # use item_d() to create all item instances
         cls.all_items = item_d(start_id, num_items)
+        #Create a variable to contain all items with item_type 'weapon'
         cls.weapon_dict = weapon_d(cls.all_items)
-    def test_item_has_name(self):
+
+
+    def test_item_has_id(self):
+        """
+        Verifies that all item instances have the parameter 'id'
+        """
+
         for i in self.all_items.values():
-            att = 
+            att = hasattr(i, 'id')
+            self.assertTrue(att)
+
+    def test_item_has_name(self):
+        """
+        Verifies that all item instances have the parameter 'name'
+        """
+
+        for i in self.all_items.values():
+            att = hasattr(i, 'name')
+            self.assertTrue(att)
+
+    def test_item_has_desc(self):
+        """
+        Verifies that all item instances have the parameter 'desc'
+        """
+
+        for i in self.all_items.values():
+            att = hasattr(i, 'desc')
+            self.assertTrue(att)
+
+    def test_item_has_item_type(self):
+        """
+        Verifies that all item instances have the parameter 'item_type'
+        """
+
+        for i in self.all_items.values():
+            att = hasattr(i, 'item_type')
+            self.assertTrue(att)  
