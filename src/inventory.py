cad617aa1b370c34b08b48a877c9ef54ce14ab9d
Noved <morph8hprom@gmail.com>
2018-04-25 23:38:39 -0500

    added __iter__, __getitem__, and __setitem__ methods, as well as _add_item method.

--- src/inventory.py
+++ src/inventory.py
@@ -13,4 +13,24 @@ class Inventory():
 
     def __init__(self):
         self.items = {}
-        
+
+    def __iter__(self):
+        # Defines item as an iterable
+        return iter(self.items.items())
+
+    def __contains__(self, item):
+        # Overwrites __contains__ method for easy use of 'if x in items'
+        return item.id in self.items
+
+    def __getitem__(self, item):
+        # Overwrites __getitem__ method for easy access to individual items
+        return self.items[item.id]
+
+    # def _get_quant(self, item_id):
+    #     # Returns the quantity of item
+    #     quant  = self.items[item_id][quant]
+    #     return quant
+
+    def _add_item(self, item):
+        # Adds item to inventory
+        self.items[item.id[item]] = item
