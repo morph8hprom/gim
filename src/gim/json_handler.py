e679adb94b362efc131c45f470acb6cccabf04fc
Noved <morph8hprom@gmail.com>
2018-08-15 20:10:40 -0500

    Added class definition for DirectoryFiles and method using glob to determine next digit for data file

--- src/gim/json_handler.py
+++ src/gim/json_handler.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python3
 
 import json
+import glob
+
+class DirectoryFiles():
+    def __init__(self, extension = 'json'):
+        self._extension = extension
+        self._files = None
+        self._digits = []
+        self._next = None
+        self._update_files()
+        self._isolate_digits()
+        self._get_next()
+
+    def _update_files(self):
+        self._files = glob.glob('data/*.{}'.format(self._extension))
+        self._files.sort()
+
+    def _isolate_digits(self):
+        for filename in self._files:
+            digit = ''
+            for c in filename:
+                if c.isdigit():
+                    digit += c
+            self._digits.append(int(digit))
+
+    def _get_next(self):
+        self._next = str(self._digits[-1] + 1)
+
 
 
 class JsonHandler():
@@ -17,7 +44,7 @@ class ItemData():
         self._dict = {}
         self._filename = None
         self._json_dump = None
+        self._to_json()
 
-
-    def to_json(self):
+    def _to_json(self):
         self._json_dump = json.dumps(self._dict)
