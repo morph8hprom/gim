e7bbd48f0df44526fc8ccc3d7fa49f65e93e0dbc
Noved <morph8hprom@gmail.com>
2018-08-17 11:01:28 -0500

    json_handler-Added glob for managing directory.  Moved hardcoded directory to variable. command-Updated import statements and init attributes.  Started work on functionality for loading all print messages from json file.

--- src/gim/command.py
+++ src/gim/command.py
@@ -3,6 +3,26 @@
 import os
 import json
 import cmd2
+from pkg_resources import resource_string
+from gim import json_handler as jh
+from gim import item
 
 class Command(cmd2.Cmd):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.prompt = '>'
+        self._msgs_file = 'data/cmd_msgs.json'
+        self._msgs = {}
+        self._all_files = None
+        self._current_file = None
+        # Load all print messages into a dictionary for easy access
+        self._load_msgs()
+
+    def _load_msgs(self):
+        jsontext = resource_string(__name__, self._msgs_file)
+        d = json.loads(jsontext.decode('utf-8'))
+        self._msgs = d
+
+
+    def _startup(self):
+        pass
--- src/gim/json_handler.py
+++ src/gim/json_handler.py
@@ -4,17 +4,22 @@ import json
 import glob
 
 class DirectoryFiles():
-    def __init__(self, extension = 'json'):
+    def __init__(self, directory = 'data/', filename = '*' extension = '.json'):
+        self._directory = directory
+        self._filename = filename
         self._extension = extension
         self._files = None
         self._digits = []
         self._next = None
+        # Build a list of all files in the data directory
         self._update_files()
+        # Isolate only the digits in the filenames
         self._isolate_digits()
+        # Store digit for next file in self._next
         self._get_next()
 
     def _update_files(self):
-        self._files = glob.glob('data/*.{}'.format(self._extension))
+        self._files = glob.glob('{}{}{}'.format(self._directory, self._filename, self._extension))
         self._files.sort()
 
     def _isolate_digits(self):
@@ -23,7 +28,7 @@ class DirectoryFiles():
             for c in filename:
                 if c.isdigit():
                     digit += c
-            self._digits.append(int(digit))
+            self._digits.append(int(digit))    
 
     def _get_next(self):
         self._next = str(self._digits[-1] + 1)
